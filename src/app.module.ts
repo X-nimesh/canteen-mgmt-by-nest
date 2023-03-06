df35a9abb335fc4924629e6d988c9712e4a2d8ff
X-nimesh <X.nimesh7@gmail.com>
2023-03-06 19:04:43 +0545

    some changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { OrmModule } from './modules/mikroOrm/orm.module';
 @Module({
   imports: [ProductModule, OrmModule],
--- src/entities/demo.ts
+++ src/entities/demo.ts
@@ -3,9 +3,13 @@ import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 @Entity()
 export class DemoTable {
   @PrimaryKey()
-  userId: number;
+  userId!: number;
   @Property()
   name: string;
   @Property()
   message: string;
+  constructor(name: string, message: string) {
+    this.name = name;
+    this.message = message;
+  }
 }
